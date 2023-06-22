9a6fb1e319acea695276b9b60295a1274b171667
PinalesRamirez <110870263+PinalesRamirez@users.noreply.github.com>
2023-06-21 21:18:49 -0700

    primera subirda

---
+++ main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:false,
+            loop:true
+        }
+    }
+})
